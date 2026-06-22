e4f7d92d4c21432068d1430928114a9d875d779c
6blCTPOmiha <bystrovmiha@gmail.com>
2026-06-23 00:56:01 +0400

    добавлена проверка на наличие необходимых полей

--- tests/ya_disk/test_disk_info_negative.py
+++ tests/ya_disk/test_disk_info_negative.py
@@ -14,3 +14,8 @@ class TestDiskInfoNegative:
     def test_get_disk_info_no_token(self, ya_api_no_token):
         response = ya_api_no_token.get(YA_DISK_ENDPOINT)
         assert response.status_code == 401, "Ожидался статус 401 Unauthorized"
+
+        body = response.json()
+        assert body["error"] is not None, "Поле error отсутствует"
+        assert body["description"] is not None, "Поле description отсутствует"
+        assert body["message"] is not None, "Поле message отсутствует"
