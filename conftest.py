e0422b701cdbdc559348e5d5b38cce80f9cca1e8
6blCTPOmiha <bystrovmiha@gmail.com>
2026-06-23 00:38:05 +0400

    2 фикстуры для новых тестов

--- conftest.py
+++ conftest.py
@@ -1,7 +1,10 @@
 import pytest
 
+from helpers.api_helper import ApiHelper
 from helpers.db_helper import DbHelper
 from helpers.wordpress_api import WordPressApi
+from helpers.yandex_disk_api import YandexDiskApi
+from config.base_config import Config
 
 
 
@@ -17,3 +20,17 @@ def db() -> DbHelper:
     helper = DbHelper()
     yield helper
     helper.close_connection()
+
+
+@pytest.fixture(scope="session")
+def ya_api() -> YandexDiskApi:
+    client = YandexDiskApi()
+    yield client
+    client.close()
+
+
+@pytest.fixture(scope="session")
+def ya_api_no_token() -> ApiHelper:
+    client = ApiHelper(base_url=Config.YA_DISK_BASE_URL)
+    yield client
+    client.close()
