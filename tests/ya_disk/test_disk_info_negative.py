e6d2d35e4a27b8a429a9db12b6fd391fcd1997bf
6blCTPOmiha <bystrovmiha@gmail.com>
2026-06-23 00:40:57 +0400

    модуль негативных тестов для яндекс диска

---
+++ tests/ya_disk/test_disk_info_negative.py
@@ -0,0 +1,16 @@
+import allure
+import pytest
+
+from data.test_data import YA_DISK_ENDPOINT
+
+
+@allure.epic("Yandex Disk API")
+@allure.feature("Disk Info")
+class TestDiskInfoNegative:
+
+    @pytest.mark.ya_disk
+    @allure.story("Получение информации о диске")
+    @allure.title("TC-YA-02: GET /v1/disk без токена")
+    def test_get_disk_info_no_token(self, ya_api_no_token):
+        response = ya_api_no_token.get(YA_DISK_ENDPOINT)
+        assert response.status_code == 401, "Ожидался статус 401 Unauthorized"
