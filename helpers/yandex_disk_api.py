bfaabd186609b6c99a24e3199d1c75dcf58c66db
6blCTPOmiha <bystrovmiha@gmail.com>
2026-06-23 13:36:49 +0400

    Теперь токен передаётся при инициализации. Добавлен статический метод парсинга ответа от сервера

--- helpers/yandex_disk_api.py
+++ helpers/yandex_disk_api.py
@@ -7,11 +7,15 @@ from data.test_data import YA_DISK_ENDPOINT
 
 class YandexDiskApi(ApiHelper):
 
-    def __init__(self):
+    def __init__(self, token: str = Config.YA_DISK_TOKEN):
         super().__init__(
             base_url=Config.YA_DISK_BASE_URL,
-            token=Config.YA_DISK_TOKEN,
+            token=token,
         )
 
     def get_disk_info(self) -> requests.Response:
         return self.get(YA_DISK_ENDPOINT)
+
+    @staticmethod
+    def parse_disk_info_response(response: requests.Response) -> tuple[int, dict]:
+        return response.status_code, response.json()
