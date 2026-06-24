869a23632db509341007f662b479517b1e517284
6blCTPOmiha <bystrovmiha@gmail.com>
2026-06-24 19:23:43 +0400

    фикстуры для новых тестов

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ import pytest
 from helpers.db_helper import DbHelper
 from helpers.wordpress_api import WordPressApi
 from helpers.yandex_disk_api import YandexDiskApi
-
+from data.test_data import FolderData
 
 
 @pytest.fixture(scope="session")
@@ -32,3 +32,32 @@ def ya_api_no_token() -> YandexDiskApi:
     client = YandexDiskApi(token="")
     yield client
     client.close()
+
+
+@pytest.fixture
+def folder_for_delete(ya_api) -> str:
+    ya_api.create_folder(FolderData.TEST_FOLDER_PATH)
+    yield FolderData.TEST_FOLDER_PATH
+    ya_api.clear_trash()
+
+
+@pytest.fixture
+def folder_for_restore(ya_api) -> str:
+    ya_api.create_folder(FolderData.TEST_FOLDER_PATH)
+    ya_api.delete_resource(FolderData.TEST_FOLDER_PATH)
+    trash_name = ya_api.find_in_trash(FolderData.TEST_FOLDER_PATH)
+    yield trash_name
+    ya_api.delete_resource(FolderData.TEST_FOLDER_PATH, permanently=True)
+
+
+@pytest.fixture
+def folder_for_create(ya_api):
+    yield FolderData.TEST_FOLDER_PATH
+    ya_api.delete_resource(FolderData.TEST_FOLDER_PATH, permanently=True)
+
+
+@pytest.fixture
+def existing_folder(ya_api):
+    ya_api.create_folder(FolderData.TEST_FOLDER_PATH)
+    yield FolderData.TEST_FOLDER_PATH
+    ya_api.delete_resource(FolderData.TEST_FOLDER_PATH, permanently=True)
