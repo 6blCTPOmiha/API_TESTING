e9309173f1c629f424d9e6dbcab0bf69643ef3bd
6blCTPOmiha <bystrovmiha@gmail.com>
2026-07-03 14:59:26 +0400

    Добавлены новые модели работающие через asdict

--- data/models.py
+++ data/models.py
@@ -1,33 +1,80 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
+
+
+@dataclass
+class UploadLinkRequest:
+    path: str
+    overwrite: str = "true"
+
+    def to_params(self) -> dict:
+        return asdict(self)
+
+
+@dataclass
+class CopyResourceRequest:
+    path: str
+    overwrite: str = "false"
+
+    def to_params(self, from_path: str) -> dict:
+        params = asdict(self)
+        params["from"] = from_path
+        return params
+
+
+@dataclass
+class DownloadLinkRequest:
+    path: str
+
+    def to_params(self) -> dict:
+        return asdict(self)
+
+
+@dataclass
+class CreateFolderRequest:
+    path: str
+
+    def to_params(self) -> dict:
+        return asdict(self)
+
+
+@dataclass
+class DeleteResourceRequest:
+    path: str
+    permanently: bool = False
+
+    def to_params(self) -> dict:
+        params = asdict(self)
+        params["permanently"] = str(self.permanently).lower()
+        return params
 
 
 @dataclass
 class UploadResponse:
-    method: str
     href: str
+    method: str
     templated: bool
 
     @classmethod
     def from_dict(cls, data: dict) -> "UploadResponse":
         return cls(
-            method=data["method"],
             href=data["href"],
-            templated=data["templated"]
+            method=data["method"],
+            templated=data.get("templated", False),
         )
 
 
 @dataclass
 class ResourceResponse:
-    method: str
     href: str
+    method: str
     templated: bool
 
     @classmethod
     def from_dict(cls, data: dict) -> "ResourceResponse":
         return cls(
-            method=data["method"],
             href=data["href"],
-            templated=data["templated"]
+            method=data["method"],
+            templated=data.get("templated", False),
         )
 
 
