1a7ebe28d20601b4bc5a4330f8662f34342164de
6blCTPOmiha <bystrovmiha@gmail.com>
2026-07-03 15:00:52 +0400

    Фикс старых тестов. Теперь они тоже используют датаклассы

--- tests/ya_disk/test_folders_negative.py
+++ tests/ya_disk/test_folders_negative.py
@@ -3,6 +3,7 @@ import pytest
 
 from data.test_data import FolderData
 from utils.parsers import parse_response
+from data.models import CreateFolderRequest, DeleteResourceRequest
 
 
 @allure.epic("Yandex Disk API")
@@ -13,7 +14,7 @@ class TestFoldersNegative:
     @allure.story("Создание папки")
     @allure.title("TC-FOL-02: PUT /v1/disk/resources — создание уже существующей папки")
     def test_create_folder_already_exists(self, ya_api, existing_folder):
-        status_code, body = parse_response(ya_api.create_folder(existing_folder))
+        status_code, body = parse_response(ya_api.create_folder(CreateFolderRequest(path=existing_folder)))
         assert status_code == 409, "Ожидался статус 409 Conflict"
         assert body["error"] == FolderData.ALREADY_EXISTS_ERROR
 
@@ -22,7 +23,7 @@ class TestFoldersNegative:
     @allure.story("Удаление папки")
     @allure.title("TC-FOL-04: DELETE /v1/disk/resources — удаление несуществующей папки")
     def test_delete_folder_not_existing(self, ya_api):
-        status_code, body = parse_response(ya_api.delete_resource(FolderData.NONEXISTENT_PATH))
+        status_code, body = parse_response(ya_api.delete_resource(DeleteResourceRequest(path=FolderData.NONEXISTENT_PATH)))
         assert status_code == 404, "Ожидался статус 404 Not Found"
         assert body["error"] == FolderData.NOT_FOUND_ERROR
 
