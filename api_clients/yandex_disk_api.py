f815979d15d622409567138de107c0047fa33528
6blCTPOmiha <bystrovmiha@gmail.com>
2026-07-03 15:00:12 +0400

    Новые модели применяются через классы

--- api_clients/yandex_disk_api.py
+++ api_clients/yandex_disk_api.py
@@ -11,6 +11,12 @@ from data.test_data import (
     YA_DISK_RESOURCES_ENDPOINT,
     YA_DISK_TRASH_ENDPOINT,
     YA_DISK_TRASH_RESTORE_ENDPOINT)
+from data.models import (
+    UploadLinkRequest,
+    CopyResourceRequest,
+    DownloadLinkRequest,
+    CreateFolderRequest,
+    DeleteResourceRequest)
 
 
 class YandexDiskApi(ApiHelper):
@@ -24,11 +30,11 @@ class YandexDiskApi(ApiHelper):
     def get_disk_info(self) -> requests.Response:
         return self.get(YA_DISK_ENDPOINT)
 
-    def create_folder(self, path: str) -> requests.Response:
-        return self.put(YA_DISK_RESOURCES_ENDPOINT, params={"path": path})
+    def create_folder(self, request: CreateFolderRequest) -> requests.Response:
+        return self.put(YA_DISK_RESOURCES_ENDPOINT, params=request.to_params())
 
-    def delete_resource(self, path: str, permanently: bool = False) -> requests.Response:
-        return self.delete(YA_DISK_RESOURCES_ENDPOINT, params={"path": path, "permanently": str(permanently).lower()})
+    def delete_resource(self, request: DeleteResourceRequest) -> requests.Response:
+        return self.delete(YA_DISK_RESOURCES_ENDPOINT, params=request.to_params())
 
     def get_trash_resources(self) -> requests.Response:
         return self.get(YA_DISK_TRASH_ENDPOINT, params={"path": "/"})
@@ -39,21 +45,21 @@ class YandexDiskApi(ApiHelper):
     def clear_trash(self) -> requests.Response:
         return self.delete(YA_DISK_TRASH_ENDPOINT)
 
-    def get_upload_link_response(self, path: str) -> requests.Response:
-        return self.get(YA_DISK_UPLOAD_ENDPOINT, params={"path": path, "overwrite": "true"})
+    def get_upload_link_response(self, request: UploadLinkRequest) -> requests.Response:
+        return self.get(YA_DISK_UPLOAD_ENDPOINT, params=request.to_params())
 
     def upload_file(self, upload_url: str, file_path: str) -> requests.Response:
         with open(file_path, "rb") as f:
             return self.put_by_url(upload_url, data=f)
 
-    def copy_resource(self, from_path: str, to_path: str) -> requests.Response:
+    def copy_resource(self, request: CopyResourceRequest, from_path: str) -> requests.Response:
         return self.post(
             YA_DISK_COPY_ENDPOINT,
-            params={"from": from_path, "path": to_path}
+            params=request.to_params(from_path)
         )
 
-    def get_download_link(self, path: str) -> requests.Response:
-        return self.get(YA_DISK_DOWNLOAD_ENDPOINT, params={"path": path})
+    def get_download_link(self, request: DownloadLinkRequest) -> requests.Response:
+        return self.get(YA_DISK_DOWNLOAD_ENDPOINT, params=request.to_params())
 
     def download_file(self, download_url: str) -> requests.Response:
         return self.get_by_url(download_url)
