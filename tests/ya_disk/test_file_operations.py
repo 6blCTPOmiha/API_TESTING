76f955f95b67cbd9cdffe48ab4d04e19c16899a8
6blCTPOmiha <bystrovmiha@gmail.com>
2026-07-03 15:02:09 +0400

    В тестах теперь идёт взаимодействие с новыми датаклассами

--- tests/ya_disk/test_file_operations.py
+++ tests/ya_disk/test_file_operations.py
@@ -1,10 +1,17 @@
 import allure
 import pytest
 
-from data.models import UploadResponse, ResourceResponse, ErrorResponse
 from data.test_data import FileData
 from utils.file_helper import FileHelper
 from utils.parsers import parse_response
+from data.models import (
+    UploadLinkRequest,
+    CopyResourceRequest,
+    DownloadLinkRequest,
+    UploadResponse,
+    ResourceResponse,
+    ErrorResponse,
+)
 
 
 @allure.epic("Yandex Disk API")
@@ -15,12 +22,11 @@ class TestFileOperations:
     @allure.story("Загрузка и копирование файла")
     @allure.title("TC-3: Загрузка data.txt в input_data и копирование в output_data")
     def test_upload_and_copy_file(self, ya_api, upload_setup):
-
         local_file_path = upload_setup
         FileHelper.generate_data_txt(local_file_path)
 
         upload_link_status, upload_link_body = parse_response(
-            ya_api.get_upload_link_response(FileData.INPUT_FILE_PATH)
+            ya_api.get_upload_link_response(UploadLinkRequest(path=FileData.INPUT_FILE_PATH))
         )
         assert upload_link_status == 200, "Ожидался статус 200 при получении ссылки на загрузку"
 
@@ -29,21 +35,22 @@ class TestFileOperations:
         resp = ya_api.upload_file(upload_obj.href, local_file_path)
         assert resp.status_code == 201, "Ожидался статус 201 при загрузке файла"
 
-
         copy_status, copy_body = parse_response(
-            ya_api.copy_resource(FileData.INPUT_FILE_PATH, FileData.OUTPUT_FILE_PATH)
+            ya_api.copy_resource(CopyResourceRequest(path=FileData.OUTPUT_FILE_PATH),
+                                 from_path=FileData.INPUT_FILE_PATH)
         )
         assert copy_status == 201, "Ожидался статус 201 при копировании файла"
-        
+
         copy_obj = ResourceResponse.from_dict(copy_body)
         assert copy_obj.href, "В ответе на копирование отсутствует поле href"
         assert copy_obj.method, "В ответе на копирование отсутствует поле method"
 
-
         conflict_status, conflict_body = parse_response(
-            ya_api.copy_resource(FileData.INPUT_FILE_PATH, FileData.OUTPUT_FILE_PATH)
+            ya_api.copy_resource(CopyResourceRequest(path=FileData.OUTPUT_FILE_PATH),
+                                 from_path=FileData.INPUT_FILE_PATH)
         )
         assert conflict_status == 409, "Ожидался статус 409 при повторном копировании"
+
         error = ErrorResponse.from_dict(conflict_body)
         assert error.error == FileData.ALREADY_EXISTS_ERROR
         assert error.description, "Поле description отсутствует в ответе 409"
@@ -56,7 +63,7 @@ class TestFileOperations:
         local_file_path = download_setup
 
         download_link_status, download_link_body = parse_response(
-            ya_api.get_download_link(FileData.SDET_FILE_PATH)
+            ya_api.get_download_link(DownloadLinkRequest(path=FileData.SDET_FILE_PATH))
         )
         assert download_link_status == 200, "Ожидался статус 200 при получении ссылки на скачивание"
 
